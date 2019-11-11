9fb1606129ca66bfb1b8dc76fafb754717a54ff0
Yuliya Bondarenko <yulyabondarenko93@gmail.com>
2019-11-11 12:42:17 +0200

    Data Binding

--- app/src/main/java/com/jubee/bookstore/BookAdapter.kt
+++ app/src/main/java/com/jubee/bookstore/BookAdapter.kt
@@ -1,30 +1,34 @@
 package com.jubee.bookstore
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.jubee.bookstore.model.BookModel
-import kotlinx.android.synthetic.main.book_list_item.view.*
+import androidx.databinding.DataBindingUtil
+import com.jubee.bookstore.databinding.BookListItemBinding
+
 
 public class BookAdapter(private val items: List<BookModel>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<BookAdapter.BookItemViewHolder>() {
 
     override fun getItemCount() = items.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return object : RecyclerView.ViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.book_list_item,
-                parent,
-                false
-            )
-        ) {}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = BookListItemBinding.inflate(inflater, parent, false)
+        return BookItemViewHolder(binding.root)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder.itemView.bookName?.text = items[position].name
-        holder.itemView.bookPrice?.text = items[position].price.toString()
-        holder.itemView.bookAbsent?.text = items[position].absent.toString()
+    override fun onBindViewHolder(holder:  BookItemViewHolder, position: Int) {
+        holder.binding?.book=items[position]
+    }
+
+    inner class BookItemViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var binding: BookListItemBinding? = null
+
+        init {
+            binding = DataBindingUtil.bind(v)
+        }
     }
 }
-
