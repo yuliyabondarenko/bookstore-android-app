d4033a151c74156acb9b63cf79f99a5778766d8f
Yuliya Bondarenko <yulyabondarenko93@gmail.com>
2019-12-10 17:25:20 +0200

    get book details from service

--- app/src/main/java/com/jubee/bookstore/BookDetailsActivity.kt
+++ app/src/main/java/com/jubee/bookstore/BookDetailsActivity.kt
@@ -2,7 +2,12 @@ package com.jubee.bookstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jubee.bookstore.model.BookModel
+import com.jubee.bookstore.service.NetworkClient
 import kotlinx.android.synthetic.main.activity_book_details.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BookDetailsActivity : AppCompatActivity() {
 
@@ -10,6 +15,24 @@ class BookDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
 
-        bookDetailsHelloText.text = "book id: " + intent.getLongExtra(BOOK_ID_EXTRA, 0)
+        val bookId = intent.getLongExtra(BOOK_ID_EXTRA, 0)
+
+        NetworkClient.getBookApiService().getBook(bookId)
+            .enqueue(object : Callback<BookModel> {
+                override fun onResponse(
+                    call: Call<BookModel>,
+                    response: Response<BookModel>
+                ) {
+
+                    val bookModel = response.body()
+
+                    bookDetailsText.text = bookModel.toString()
+                }
+
+                override fun onFailure(call: Call<BookModel>, t: Throwable) {
+                    bookDetailsText.text = getString(R.string.common_error_msg) + t.localizedMessage;
+                }
+
+            })
     }
 }
--- app/src/main/java/com/jubee/bookstore/api/BookApiService.kt
+++ app/src/main/java/com/jubee/bookstore/api/BookApiService.kt
@@ -1,7 +1,9 @@
 package com.jubee.bookstore.api
 
+import com.jubee.bookstore.model.BookModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BookApiService {
@@ -11,4 +13,7 @@ interface BookApiService {
         @Query("page") page: Int?,
         @Query("sort") sort: String?
     ): Call<BookCollectionApiResponse>
+
+    @GET("books/{id}")
+    fun getBook(@Path("id") id: Long): Call<BookModel>
 }
