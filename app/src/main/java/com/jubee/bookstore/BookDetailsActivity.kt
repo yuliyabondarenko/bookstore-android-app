3eace734658428a3e7d7172ca1ecab8d4b132d2a
Yuliya Bondarenko <yulyabondarenko93@gmail.com>
2019-12-15 18:14:12 +0200

    i/1 Add 'back' button in BookDetailsActivity

--- app/src/main/java/com/jubee/bookstore/BookDetailsActivity.kt
+++ app/src/main/java/com/jubee/bookstore/BookDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.jubee.bookstore
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.lifecycle.Observer
@@ -8,12 +9,14 @@ import androidx.lifecycle.ViewModelProviders
 import com.jubee.bookstore.databinding.ActivityBookDetailsBinding
 import com.jubee.bookstore.viewmodel.BookViewModel
 
+
 class BookDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBookDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = setContentView(this, R.layout.activity_book_details)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val bookId = intent.getLongExtra(BOOK_ID_EXTRA, 0)
 
@@ -22,5 +25,15 @@ class BookDetailsActivity : AppCompatActivity() {
         bookViewModel.bookLiveData.observe(this, Observer { book ->
             binding.book = book
         })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
